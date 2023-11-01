d7245bdbd8ef1bc612a252dfdcaef2d9ca0931c3
SoffR <sofia.b.rovati.f@gmail.com>
2023-10-31 22:23:37 -0300

    js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-// Mi código JavaScript:
+const yearSelector = document.querySelector("#years");
+const brandSelector = document.querySelector("#brands");
+const modelSelector = document.querySelector("#models");
+const statusSelector = document.querySelector("#status");
+
+for (let i = 1900; i <= 2023; i++) {
+  yearSelector.insertAdjacentHTML += `<option value="${i}">${i}</option>`;
+}
+
+// Autos
+
+fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (json) {})
+  .catch(function (err) {
+    console.log("Error");
+  });
+
+// Marcas
+
+fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (json) {})
+  .catch(function (err) {
+    console.log("Error");
+  });
+
+// Modelos
+
+fetch("https://ha-front-api-proyecto-final.vercel.app/models?brand=Audi")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (json) {})
+  .catch(function (err) {
+    console.log("Error");
+  });
