afb1dc5aee6405e54e9ae72327789d4685f347eb
AlexAlonsoAN <146207395+AlexAlonsoDw@users.noreply.github.com>
2023-11-03 21:44:50 -0300

    cambios temporales

--- js/app.js
+++ js/app.js
@@ -3,10 +3,21 @@ const brandSelector = document.querySelector("#brands");
 const modelSelector = document.querySelector("#models");
 const statusSelector = document.querySelector("#status");
 
-const cardColumn = document.querySelector("#cardColumn")
-const valorActualDeBrand = ""
+const cardColumn = document.querySelector("#cardColumn");
+const valorActualDeBrand = "";
 
-const DataCards = []
+const DataCards = [];
+
+
+
+const star =`<i class="bi bi-star-fill">`
+
+
+
+;
+
+
+
 
 
 // Autos
@@ -16,21 +27,40 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
     return res.json();
   })
   .then(function (json) {
-    json.forEach(element => {
-    
+    json.forEach((element) => {
+
+
       cardColumn.innerHTML += `
 <div class="card mb-3">
-<div class="row g-0">
-  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 col-lg-4">
+<div class="row d-flex justify-content-center ">
+
+  <div id="imgDiv" class=" col-sm-10 col-md-4 col-lg-2 col-lg-4">
     <img
       src="${element.image}"
       class="car img-fluid"
       alt="..."
     />
+      
   </div>
-  <div class="col-md-6 col-lg-6 card-info">
+
+  <div id="contenido-card" class=" col-md-6 col-lg-8 card-info">
     <div class="card-description">
-      <h5 class="card-title">${element.model}</h5>
+    <div  class = "cardDates ">
+    <h5 class="card-title">${element.model}</h5>
+
+    <div class="subTitle-container">
+    <p>${element.year} | USD ${element.price_usd} | <p>  ${star}</p>
+  
+
+     
+    </p>
+
+  </div>
+    </div>
+
+     
+    </div>
+
       <p class="card-info">${element.description}
       </p>
       <div class="card-buttons mt-2">
@@ -47,11 +77,8 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
     </div>
   </div>
 </div>
-</div>`
-
-      
+</div>`;
     });
-
   })
   .catch(function (err) {
     console.log("Error");
@@ -65,15 +92,11 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
   })
   .then(function (json) {
     for (const i of json) {
-      const option = document.createElement("option")
-      option.text =i
-      option.value =i
-      brandSelector.add(option)
- 
-
-
+      const option = document.createElement("option");
+      option.text = i;
+      option.value = i;
+      brandSelector.add(option);
     }
-   
   })
   .catch(function (err) {
     console.log("Error");
@@ -82,39 +105,35 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
 // Modelos
 
 brandSelector.addEventListener("change", function () {
+  fetch(
+    "https://ha-front-api-proyecto-final.vercel.app/models?brand=" +
+      brandSelector.value
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (json) {
+      modelSelector.innerHTML = "";
+      for (const i of json) {
+        console.log(i);
 
-fetch("https://ha-front-api-proyecto-final.vercel.app/models?brand="+brandSelector.value)
-.then(function (res) {
-  return res.json();
-})
-.then(function (json) {
-  modelSelector.innerHTML = ""
-  for (const i of json) {
-    console.log(i);
-
-    const option = document.createElement("option")
-    option.text =i
-    option.value =i
-    modelSelector.add(option)
-
-
-
-  }
- 
-})
-.catch(function (err) {
-  console.log("Error");
+        const option = document.createElement("option");
+        option.text = i;
+        option.value = i;
+        modelSelector.add(option);
+      }
+    })
+    .catch(function (err) {
+      console.log("Error");
+    });
 });
-})
-
 
 /* status */
 
-for( i=0; i <=1 ; i++ ){
-
-  const option = document.createElement("option")
-  option.text =i
-  statusSelector.add(option)
+for (i = 0; i <= 1; i++) {
+  const option = document.createElement("option");
+  option.text = i;
+  statusSelector.add(option);
 }
 
 /* status */
@@ -123,19 +142,17 @@ for( i=0; i <=1 ; i++ ){
 
 /* year */
 
-for( i=1900; i <=2023 ; i++ ){
-
-  const option = document.createElement("option")
-  option.text =i
-  yearSelector.add(option)
+for (i = 1900; i <= 2023; i++) {
+  const option = document.createElement("option");
+  option.text = i;
+  yearSelector.add(option);
 }
 
 /* year */
 
 /* filtro */
 
-  /* formulario logic */
-  /* formulario logic */
+/* formulario logic */
+/* formulario logic */
 
-  /* modelo selecion actual */
-
+/* modelo selecion actual */
