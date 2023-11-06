7637b2e87398b7b5257b5621565505c160cdab95
Erika Planchesteiner <eplanchesteiner@gmail.com>
2023-11-06 18:53:35 -0300

    Revert "Merge branch 'main' of https://github.com/ErikaPlanch/ProyectoFinal-AutoVIP"
    This reverts commit 2ac75040bc6c20f935def387d6641c55b0c040ba, reversing
    changes made to 6d957403fedfa87d19dff8c231e494a1fa95fade.

--- js/app.js
+++ js/app.js
@@ -3,13 +3,11 @@ const brandSelector = document.querySelector("#brands");
 const modelSelector = document.querySelector("#models");
 const statusSelector = document.querySelector("#status");
 
-const cardColumn = document.querySelector("#cardColumn");
-const valorActualDeBrand = "";
+const cardColumn = document.querySelector("#cardColumn")
+const valorActualDeBrand = ""
 
-const DataCards = [];
+const DataCards = []
 
-const star =  `<img src="img/cars_sales/star_rating.svg" alt="Search Icon" />`
-const starWhite = `<img src="img/cars_sales/star_rating-withe.svg" alt="Search Icon" />`;
 
 // Autos
 
@@ -18,51 +16,21 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
     return res.json();
   })
   .then(function (json) {
-    json.forEach((element) => {
-let contenedorStars =""
-for (let i = 0; i < element.rating; i++) {
- contenedorStars += star
-  
-}
-let contadorRating = 5-element.rating
-  for (let i = 0; i < contadorRating; i++) {
-    contenedorStars += starWhite
-     
-   }
-
-
-
+    json.forEach(element => {
+    
       cardColumn.innerHTML += `
 <div class="card mb-3">
-<div class="row d-flex justify-content-center ">
-
-  <div id="imgDiv" class=" col-sm-10 col-md-4 col-lg-2 col-lg-4">
+<div class="row g-0">
+  <div class="col-6 col-lg-4">
     <img
       src="${element.image}"
-      class="car img-fluid"
+      class="img-fluid"
       alt="..."
     />
-      
   </div>
-
-  <div id="contenido-card" class=" col-md-6 col-lg-8 card-info">
+  <div class="col-md-6 col-lg-6 card-info">
     <div class="card-description">
-    <div  class = "cardDates ">
-    <h5 class="card-title">${element.model}</h5>
-
-    <div class="subTitle-container">
-    <p>${element.year} | USD ${element.price_usd} | <p>  ${contenedorStars}</p>
-  
-
-     
-    </p>
-
-  </div>
-    </div>
-
-     
-    </div>
-
+      <h5 class="card-title">${element.model}</h5>
       <p class="card-info">${element.description}
       </p>
       <div class="card-buttons mt-2">
@@ -79,8 +47,11 @@ let contadorRating = 5-element.rating
     </div>
   </div>
 </div>
-</div>`;
+</div>`
+
+      
     });
+
   })
   .catch(function (err) {
     console.log("Error");
@@ -94,11 +65,15 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
   })
   .then(function (json) {
     for (const i of json) {
-      const option = document.createElement("option");
-      option.text = i;
-      option.value = i;
-      brandSelector.add(option);
+      const option = document.createElement("option")
+      option.text =i
+      option.value =i
+      brandSelector.add(option)
+ 
+
+
     }
+   
   })
   .catch(function (err) {
     console.log("Error");
@@ -107,54 +82,60 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
 // Modelos
 
 brandSelector.addEventListener("change", function () {
-  fetch(
-    "https://ha-front-api-proyecto-final.vercel.app/models?brand=" +
-      brandSelector.value
-  )
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (json) {
-      modelSelector.innerHTML = "";
-      for (const i of json) {
-        console.log(i);
 
-        const option = document.createElement("option");
-        option.text = i;
-        option.value = i;
-        modelSelector.add(option);
-      }
-    })
-    .catch(function (err) {
-      console.log("Error");
-    });
-});
+fetch("https://ha-front-api-proyecto-final.vercel.app/models?brand="+brandSelector.value)
+.then(function (res) {
+  return res.json();
+})
+.then(function (json) {
+  modelSelector.innerHTML = ""
+  for (const i of json) {
+    console.log(i);
 
-/* status */
-
- 
+    const option = document.createElement("option")
+    option.text =i
+    option.value =i
+    modelSelector.add(option)
 
 
-/* status */
 
-/* filtro */
-
-/* year */
-
-
-/* year */
-
-/* filtro */
-
-/* formulario logic */
-
-const submitButton = document.querySelector(".submitButton");
-
-submitButton.addEventListener("submit", function () {
-  for (const form of document.getElementsByTagName("form")) {
-    form.reset();
   }
+ 
+})
+.catch(function (err) {
+  console.log("Error");
 });
-/* formulario logic */
+})
 
-/* modelo selecion actual */
+
+/* status */
+
+for( i=0; i <=1 ; i++ ){
+
+  const option = document.createElement("option")
+  option.text =i
+  statusSelector.add(option)
+}
+
+/* status */
+
+/* filtro */
+
+/* year */
+
+for( i=1900; i <=2023 ; i++ ){
+
+  const option = document.createElement("option")
+  option.text =i
+  yearSelector.add(option)
+}
+
+/* year */
+
+/* filtro */
+
+  /* formulario logic */
+  /* formulario logic */
+
+  /* modelo selecion actual */
+
